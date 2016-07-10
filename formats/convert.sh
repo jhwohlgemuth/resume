53c17b793a359ce34427b9b0f6ccc226b1a24f48
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2016-07-09 20:19:48 -0500

    created shell script for converting resume formats

---
+++ formats/convert.sh
@@ -0,0 +1,7 @@
+pandoc -s -S resume.txt -o resume.docx
+
+pandoc -s resume.txt --template templates/resume.template.html.html -t html5 -o resume.html
+
+pandoc -s resume.txt --template templates/resume.template.tex.tex -o resume.tex
+
+pandoc resume.txt --template templates/resume.template.tex.tex -o resume.pdf
