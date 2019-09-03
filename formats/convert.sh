70c43cbab829a25a2e178cc2ca0219037bb2b41a
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2019-09-02 21:01:16 -0500

    feat: Add Adobe XD (and minor tweaks) - still need to update/upload PDF

--- formats/convert.sh
+++ formats/convert.sh
@@ -1,6 +1,6 @@
-pandoc -s -S resume.txt -o resume.docx
+pandoc -s -S resume.txt -o resume.docx --metadata pagetitle="Resume of Jason Wohlgemuth"
 
-pandoc -s resume.txt --template templates/resume.template.html.html -t html5 -o resume.html
+pandoc -s resume.txt --template templates/resume.template.html.html -t html5 -o resume.html --metadata pagetitle="Resume of Jason Wohlgemuth"
 
 pandoc -s resume.txt --template templates/resume.template.tex.tex -o resume.tex
 
