cd7962bbf0dd62f63a2b90a58a74098672805f8d
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2026-04-26 13:07:40 -0400

    fix: Remove problematic character; remove old artifacts; generate new PDF

--- Build.bat
+++ Build.bat
@@ -1,4 +1,3 @@
-REM Convert formats
 SET FORMAT=ornl
 
 cd formats/%FORMAT%
@@ -6,8 +5,5 @@ pandoc -f "markdown+smart" "resume.txt" -o "resume.docx" --metadata pagetitle="R
 pandoc -s "resume.txt" --template "templates/resume.template.html.html" -t "html5" -o "resume.html" --metadata pagetitle="Resume of Jason Wohlgemuth"
 pandoc -s "resume.txt" --template "templates/resume.template.tex.tex" -o "resume.tex"
 pandoc "resume.txt" --template "templates/resume.template.tex.tex" -o "resume.pdf"
-REM Copy HTML file
+
 Copy .\resume.html ..\..\index.html
-REM Open HTML file
-@REM npx open-cli ..\..\index.html
-pause
